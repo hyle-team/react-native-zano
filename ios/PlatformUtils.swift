e010f154791f1e9f9c62ac4adc43376c23166305
Mikhailov Anton <anion155@gmail.com>
2025-08-04 12:24:15 +0400

    feat: downloads dir

--- ios/PlatformUtils.swift
+++ ios/PlatformUtils.swift
@@ -8,8 +8,10 @@ class PlatformUtils: HybridPlatformUtilsSpec {
     let docsDir = try! fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
     return docsDir.path
   }
-  
+
   public func get_downloads_directory() throws -> String {
-    throw TODO()
+    let fileManager = FileManager.default
+    let downloadsDir = try! fileManager.url(for: .downloadsDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    return downloadsDir.path
   }
 }
